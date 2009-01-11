hfiguiere/abiword

commit 2bd9739c502bd81fc4e134d5f8a156c24ac22f0e

warning fixes.
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@25442 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/text/fmt/xp/fl_SectionLayout.cpp
@@ -465,6 +465,7 @@ bool fl_SectionLayout::bl_doclistener_insertSection(fl_ContainerLayout* pPrevL,
 		const PP_AttrProp* pHFAP = NULL;
 		PT_AttrPropIndex indexAP = pcrx->getIndexAP();
 		bool bres = (m_pDoc->getAttrProp(indexAP, &pHFAP) && pHFAP);
+		UT_UNUSED(bres);
 		UT_ASSERT(bres);
 		const gchar* pszNewID = NULL;
 		pHFAP->getAttribute("id", pszNewID);
@@ -639,6 +640,7 @@ fl_SectionLayout * fl_SectionLayout::bl_doclistener_insertTable(SectionType iTyp
 																			PL_ListenerId lid,
 																			PL_StruxFmtHandle sfhNew))
 {
+	UT_UNUSED(iType);
 	UT_ASSERT(iType == FL_SECTION_TABLE);
 	UT_return_val_if_fail(pcrx, NULL);
 	UT_ASSERT(pcrx->getType() == PX_ChangeRecord::PXT_InsertStrux);
@@ -2667,11 +2669,9 @@ void fl_DocSectionLayout::collapse(void)
 	{
 		if(pCol->countCons() > 0)
 		{
-			UT_uint32 i = 0;
 			for(i=0; i<pCol->countCons();i++)
 			{
-				fp_ContainerObject * pCon = pCol->getNthCon(i);
-				UT_DEBUGMSG(("Still have a pointer to a container %x -removing pointer \n",pCon));
+				UT_DEBUGMSG(("Still have a pointer to a container %lx -removing pointer \n",pCol->getNthCon(i)));
 				pCol->justRemoveNthCon(i);
 			}
 		}
@@ -3054,8 +3054,7 @@ void fl_DocSectionLayout::deleteOwnedPage(fp_Page* pPage, bool bReallyDeleteIt)
 	fl_DocSectionLayout * pDSL = this;
 	if(!getDocLayout()->isLayoutDeleting() && bReallyDeleteIt)
 	{
-		UT_sint32 i = m_pLayout->findPage(pPage);
-		if(i>0)
+		if(m_pLayout->findPage(pPage) > 0)
 		{
 			UT_DEBUGMSG(("fl_DocSec: deleting page %x ReallyDeleteIt %d \n",pPage,bReallyDeleteIt));
 			m_pLayout->deletePage(pPage,true);
@@ -3393,8 +3392,7 @@ UT_sint32 fl_HdrFtrSectionLayout::_findShadow(fp_Page* pPage)
 */
 void fl_HdrFtrSectionLayout::changeIntoHdrFtrSection( fl_DocSectionLayout * pSL)
 {
-	fl_DocSectionLayout* pPrevSL = pSL->getPrevDocSection();
-	UT_ASSERT(pPrevSL);
+	UT_ASSERT(pSL->getPrevDocSection());
 	// clear all the columns
 	fp_Column* pCol =NULL;
 
@@ -3473,14 +3471,14 @@ bool fl_HdrFtrSectionLayout::doclistener_deleteStrux(const PX_ChangeRecord * pcr
 			  pcr->getType()==PX_ChangeRecord::PXT_ChangeStrux);
 	
 
+	PX_ChangeRecord_StruxChange * pcrx  = (PX_ChangeRecord_StruxChange *) pcr;
+	UT_UNUSED(pcrx);
 	if(pcr->getType()==PX_ChangeRecord::PXT_ChangeStrux)
 	{
-		PX_ChangeRecord_StruxChange * pcrx  = (PX_ChangeRecord_StruxChange *) pcr;
 		UT_ASSERT_HARMLESS( pcrx->isRevisionDelete() );
 	}
 	else
 	{
-		PX_ChangeRecord_Strux * pcrx  = (PX_ChangeRecord_Strux *) pcr;
 		UT_ASSERT(pcrx->getStruxType()==PTX_SectionHdrFtr);
 	}
 	
@@ -5368,6 +5366,7 @@ bool fl_ShadowListener::populate(PL_StruxFmtHandle sfh,
 
 		{
 			const fl_Layout * pL = static_cast<const fl_Layout *>(sfh);
+			UT_UNUSED(pL);
 			UT_ASSERT(pL->getType() == PTX_Block);
 			UT_ASSERT(m_pCurrentBL == (static_cast<const fl_ContainerLayout *>(pL)));
 		}
@@ -5385,6 +5384,7 @@ bool fl_ShadowListener::populate(PL_StruxFmtHandle sfh,
 
 		{
 			const fl_Layout * pL = static_cast<const fl_Layout *>(sfh);
+			UT_UNUSED(pL);
 			UT_ASSERT(pL->getType() == PTX_Block);
 			UT_ASSERT(m_pCurrentBL == (static_cast<const fl_ContainerLayout *>(pL)));
 		}
@@ -5403,6 +5403,7 @@ bool fl_ShadowListener::populate(PL_StruxFmtHandle sfh,
 
 		{
 			const fl_Layout * pL = static_cast<const fl_Layout *>(sfh);
+			UT_UNUSED(pL);
 			UT_ASSERT(pL->getType() == PTX_Block);
 			UT_ASSERT(m_pCurrentBL == (static_cast<const fl_ContainerLayout *>(pL)));
 		}

src/af/xap/gtk/xap_UnixFrameImpl.cpp
@@ -245,8 +245,10 @@ s_loadImage (const UT_UTF8String & file, FV_View * pView, XAP_Frame * pF, gint x
 		mouseX = pView->getGraphics()->tlu(mouseX);
 	if(pView && pView->getGraphics())
 		mouseY = pView->getGraphics()->tlu(mouseY);
+#ifdef DEBUG
 	double xInch = (double) mouseX/1440.;
 	double yInch = (double) mouseY/1440.;
+#endif
 
 	UT_DEBUGMSG(("Insert Image at logical (x,y) %d %d \n",mouseX,mouseY));
 

src/af/xap/xp/xap_App.cpp
@@ -1027,7 +1027,8 @@ void XAP_App::rememberModelessId(UT_sint32 id, XAP_Dialog_Modeless * pDialog)
 
 	}
 	UT_ASSERT( i <= NUM_MODELESSID );
-	UT_ASSERT( m_IdTable[i].id == -1 );
+// This assert cause a warning because array subscript is out of bounds.
+//	UT_ASSERT( m_IdTable[i].id == -1 );
 	UT_ASSERT( pDialog);
 	m_IdTable[i].id =  id;
 	m_IdTable[i].pDialog =  pDialog;

src/text/fmt/xp/fl_FrameLayout.cpp
@@ -478,6 +478,7 @@ void fl_FrameLayout::redrawUpdate(void)
 
 bool fl_FrameLayout::doclistener_deleteStrux(const PX_ChangeRecord_Strux * pcrx)
 {
+	UT_UNUSED(pcrx);
 	UT_ASSERT(pcrx->getType()==PX_ChangeRecord::PXT_DeleteStrux);
 #if 0
 	fp_FrameContainer * pFrameC = getFirstContainer();

src/text/fmt/xp/fl_TOCLayout.cpp
@@ -1281,6 +1281,7 @@ void fl_TOCLayout::redrawUpdate(void)
 
 bool fl_TOCLayout::doclistener_deleteStrux(const PX_ChangeRecord_Strux * pcrx)
 {
+	UT_UNUSED(pcrx);
 	UT_ASSERT(pcrx->getType()==PX_ChangeRecord::PXT_DeleteStrux);
 //
 // Remove all remaining structures
@@ -1353,6 +1354,9 @@ TOCEntry * fl_TOCLayout::createNewEntry(fl_BlockLayout * pNewBL)
 		sAfter = m_sLabAfter4;
 		bInherit = m_bInherit4;
 		iStartAt = m_iStartAt4;
+	}
+	else {
+		UT_ASSERT_NOT_REACHED();
 	}
 	TOCEntry * pNew = new TOCEntry(pNewBL,m_iCurrentLevel,
 								   sDispStyle,
@@ -2300,6 +2304,7 @@ bool fl_TOCListener::populate(PL_StruxFmtHandle sfh,
 		const PX_ChangeRecord_Span * pcrs = static_cast<const PX_ChangeRecord_Span *> (pcr);
 
 		{
+			UT_UNUSED(sfh);
 			UT_ASSERT(static_cast<const fl_Layout *>(sfh)->getType() == PTX_Block);
 			UT_ASSERT(m_pCurrentBL == (static_cast<const fl_ContainerLayout *>(sfh)));
 		}

src/text/fmt/xp/fl_TableLayout.cpp
@@ -922,6 +922,7 @@ bool fl_TableLayout::bl_doclistener_insertTable( const PX_ChangeRecord_Strux * p
 																	   PL_ListenerId lid,
 																	   PL_StruxFmtHandle sfhNew))
 {
+	UT_UNUSED(iType);
 	UT_ASSERT(iType == FL_SECTION_TABLE);
 	UT_ASSERT(pcrx->getType()==PX_ChangeRecord::PXT_InsertStrux);
 	UT_ASSERT(pcrx->getStruxType()==PTX_SectionTable);
@@ -2113,6 +2114,7 @@ fp_Container* fl_CellLayout::getNewContainer(fp_Container * pPrev)
 //
 // One cell container per cell layout
 //
+	UT_UNUSED(pPrev);
 	UT_ASSERT(pPrev == NULL);
 	UT_ASSERT((getFirstContainer() == NULL) && (getLastContainer()==NULL));
 	createCellContainer();

src/text/fmt/xp/fp_Column.cpp
@@ -667,7 +667,8 @@ void fp_VerticalContainer::getScreenOffsets(fp_ContainerObject* pContainer,
 	fp_Container * pCon = static_cast<fp_Container *>(this);
 	bool bCell = false;
 	bool bTable = false;
-	UT_sint32 xcell,ycell = 0;
+	UT_sint32 xcell = 0;
+	UT_sint32 ycell = 0;
 	if((getContainerType() == FP_CONTAINER_TABLE) && (pContainer->getContainerType() == FP_CONTAINER_CELL))
 	{
 	        pCon =  static_cast<fp_Container *>(pContainer);

src/text/fmt/xp/fp_Page.cpp
@@ -2130,6 +2130,7 @@ void fp_Page::annotationHeightChanged(void)
 void fp_Page::columnHeightChanged(fp_Column* pCol)
 {
 	xxx_UT_DEBUGMSG(("SEVIOR: Column height changed \n"));
+	UT_UNUSED(pCol);
 	UT_ASSERT(m_vecColumnLeaders.findItem(pCol->getLeader()) >= 0);
 	if(breakPage())
 	{
@@ -2403,9 +2404,9 @@ void fp_Page::mapXYToPosition(bool bNotFrames,UT_sint32 x, UT_sint32 y, PT_DocPo
 		if(m_pView && m_pView->getViewMode() == VIEW_PRINT)
 		{
 			fp_ShadowContainer * hf[2] = { m_pHeader, m_pFooter };
-			for (UT_uint32 i = 0; i < G_N_ELEMENTS(hf); i++)
+			for (UT_uint32 j = 0; j < G_N_ELEMENTS(hf); j++)
 			{
-				fp_ShadowContainer * p = hf[i];
+				fp_ShadowContainer * p = hf[j];
 
 				if(p == NULL || !p->getFirstContainer())
 					continue;

commit 3bf264231082bdfd7d2324dd6c37ab1358543696

Fix an assert introduced with r25442 and a typo. Don't backport.
git-svn-id: svn+ssh://svn.abisource.com/svnroot/abiword/trunk@25504 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

src/text/fmt/xp/fl_SectionLayout.cpp
@@ -3463,7 +3463,7 @@ void fl_HdrFtrSectionLayout::changeIntoHdrFtrSection( fl_DocSectionLayout * pSL)
  * I do this because I expect that this will be called as part
  * on an undo "Insert Header" command. The rest of the undo needs blocks to
  * delete so I'm putting them there to keep the rest of the undo code happy
-\param pcrx the changerecord identifying this action as necesary.
+\param pcr the changerecord identifying this action as necesary.
 \returns true
 */
 bool fl_HdrFtrSectionLayout::doclistener_deleteStrux(const PX_ChangeRecord * pcr)
@@ -3472,14 +3472,16 @@ bool fl_HdrFtrSectionLayout::doclistener_deleteStrux(const PX_ChangeRecord * pcr
 			  pcr->getType()==PX_ChangeRecord::PXT_ChangeStrux);
 	
 
-	PX_ChangeRecord_StruxChange * pcrx  = (PX_ChangeRecord_StruxChange *) pcr;
-	UT_UNUSED(pcrx);
 	if(pcr->getType()==PX_ChangeRecord::PXT_ChangeStrux)
 	{
-		UT_ASSERT_HARMLESS( pcrx->isRevisionDelete() );
+		PX_ChangeRecord_StruxChange * pcrxc = (PX_ChangeRecord_StruxChange *) pcr;
+		UT_UNUSED(pcrxc);
+		UT_ASSERT_HARMLESS( pcrxc->isRevisionDelete() );
 	}
 	else
 	{
+		PX_ChangeRecord_Strux * pcrx = (PX_ChangeRecord_Strux *) pcr;
+		UT_UNUSED(pcrx);
 		UT_ASSERT(pcrx->getStruxType()==PTX_SectionHdrFtr);
 	}
 	