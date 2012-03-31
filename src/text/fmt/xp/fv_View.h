fdf01eb19be254eacf20dae1647ed4e69b581eee
pradeeban <pradeeban@bcba8976-2d24-0410-9c9c-aab3bd5fdfd6>
2012-03-31 12:58:59 +0000

    Bug 12862 - Running word count in the status bar. Patch from Kousik

--- src/text/fmt/xp/fv_View.h
+++ src/text/fmt/xp/fv_View.h
@@ -736,6 +736,7 @@ public:
 // ----------------------
 
 	FV_DocCount 		countWords(bool bActuallyCountWords = true);
+	UT_uint32 		countWordsStatusBar(); //When counting words from the status bar, the entire structure is not needed. Only the word count is enough
 
 // -----------------------
 
